aced63b5c464a11081a6f1b56472a9f3ba2a0947
Siddhant Ananda Patil <siddhantpatil1065@gmail.com>
2023-08-22 18:47:10 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { AdminLayoutModule } from './AdminLayout/admin-layout/admin-layout.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SingUpComponent,
+    SignInComponent,
+
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
